d0a31a1495e05508be4201fddfe6ae765e4457ce
20161104570 <a20161104570@ciecimac17deiMac.local>
2018-10-31 12:01:28 +0800

    Calculator_9
    Calculator_9

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,32 +15,60 @@ class ViewController: UIViewController {
     //var：定义一个变量 变量类型从右边推导而来
     var userIsInTheMiddleOfTypingANumber: Bool = false
     
-    var number_one:String = ""
+    var number_one:String = ""//第一个数字
 
-    var a = ""
+    var number = "" //输入
+    
+    var symbol = ""//符号
+    
+    var result:String = ""//结果
+    
+    var decimal = 0 //小数点计数
     
     @IBAction func app(_ sender: UIButton) {
         
         let digit = sender.currentTitle
         
         if userIsInTheMiddleOfTypingANumber{
-            dis.text = dis.text! + digit!
-            a = dis.text!
+           
+            number = number + digit!
+            dis.text! = number
+            
         }
         else{
-            dis.text = digit
-            a = dis.text!
+            
+            number = digit!
+             dis.text = number
             userIsInTheMiddleOfTypingANumber = true
         }
-       
+        
+        if(symbol != ""){
+            dis.text = number_one + symbol + number
+        }
+        if (symbol == "/" && number == "0"){
+            dis.text = "错误，除数不为0"
+        }
+        
     }
     
     @IBAction func clear(_ sender: UIButton) {
-        dis.text = "0"
+        dis.text = ""
+        symbol = ""
+        result = ""
+        number = ""
+        decimal = 0
     }
     
     @IBAction func point(_ sender: Any) {
-        dis.text = dis.text! + "."
+        if (decimal == 0)
+        {
+            number = number + "."
+            dis.text = number
+            decimal = 1
+        }
+        if(symbol != ""){
+            dis.text = number_one + symbol + number
+        }
     }
     var control:Int = 0 //计算符号
     var control_minus:Int = 0 //正负号
@@ -57,60 +85,73 @@ class ViewController: UIViewController {
     }
     //remove
     @IBAction func remove(_ sender: Any) {
-        
+        result.removeLast()
     }
     
     @IBAction func addition(_ sender: Any) {
         control = 1
         number_one = dis.text!
-        dis.text = number_one + "+"
-        a = ""
+        symbol = "+"
+        dis.text = number_one + symbol
+        number = ""
+        decimal = 0
     }
     
     @IBAction func subtraction(_ sender: Any) {
         control = 2
         number_one = dis.text!
-        dis.text = ""
+        symbol = "-"
+        dis.text = number_one + symbol
+        number = ""
+        decimal = 0
        
     }
     
     @IBAction func multiplication(_ sender: Any) {
         control = 3
         number_one = dis.text!
-        dis.text = ""
-        
+        symbol = "*"
+        dis.text = number_one + symbol
+        number = ""
+        decimal = 0
     }
     
     @IBAction func division(_ sender: Any) {
         control = 4
         number_one = dis.text!
-        dis.text = ""
+        symbol = "/"
+        dis.text = number_one + symbol
+        number = ""
+        decimal = 0
        
     }
     @IBAction func ca(_ sender: UIButton) {
-        var result:String = ""
+        //var result:String = ""
         //var two:String = ""
         //tow = dis.text!
         
         switch control {
         case 1 :
-            result = "\(Double(number_one)! + Double(a)!)"
+            result = "\(Double(number_one)! + Double(number)!)"
         case 2 :
-            result = "\(Double(number_one)! - Double(dis.text!)!)"
+            result = "\(Double(number_one)! - Double(number)!)"
         case 3 :
-            result = "\(Double(number_one)! * Double(dis.text!)!)"
+            result = "\(Double(number_one)! * Double(number)!)"
         case 4 :
-            result = "\(Double(number_one)! / Double(dis.text!)!)"
+            result = "\(Double(number_one)! / Double(number)!)"
         default:
             result = "0"
         }
         
+        result = String(format: "%0.8f", Double(result)!)
+        
         while (result.last == "0"){
-            result.removeLast()
+            result.removeLast() // 数组尾端移除变量值
         }
         if (result.last == "."){
             result.removeLast()
         }
+        
         dis.text = result
         control = 0
         number_one = dis.text!
